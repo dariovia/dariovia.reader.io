e405cdf05756fb1545057233095467a6f3f124a6
dariovia <dariovia@yahoo.it>
2025-02-15 11:58:30 +0100

    Update conf.js
    add conf latino

--- reader/conf.js
+++ reader/conf.js
@@ -5,7 +5,8 @@ function loadConfiguration(conf) {
 				compiti: "https://dariovia.github.io/dariovia.reader.io/reader/csv/data.csv",
 				spesa: "https://dariovia.github.io/dariovia.reader.io/reader/csv/spesa.csv",
 				altro: "https://dariovia.github.io/dariovia.reader.io/reader/csv/altro.csv",
-				chimica: "https://dariovia.github.io/dariovia.reader.io/reader/csv/chimica.csv"
+				chimica: "https://dariovia.github.io/dariovia.reader.io/reader/csv/chimica.csv",
+				latino: "https://dariovia.github.io/dariovia.reader.io/reader/csv/latino.csv"
 			}
 			break;
 		default:
