ce79519913e21bf17e7ae9937cf5e79c8d4a7801
dariovia <dariovia@yahoo.it>
2025-02-16 17:04:51 +0100

    new verbi2
    new csv verbi2

--- reader/conf.js
+++ reader/conf.js
@@ -8,6 +8,7 @@ function loadConfiguration(conf) {
 				chimica: "https://dariovia.github.io/dariovia.reader.io/reader/csv/chimica.csv",
 				latino: "https://dariovia.github.io/dariovia.reader.io/reader/csv/latino.csv",
 				verbi: "https://dariovia.github.io/dariovia.reader.io/reader/csv/verbi.csv"
+				verbi2: "https://dariovia.github.io/dariovia.reader.io/reader/csv/20250215/verbi.csv"
 			}
 			break;
 		default:
